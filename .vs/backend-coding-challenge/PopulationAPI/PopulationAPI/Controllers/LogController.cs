99343324473608a439c3afcddbedde02dc968fd4
theMonadicNomad <hello@nagacharan.me>
2022-10-10 18:54:10 +0200

    logcontroller

---
+++ .vs/backend-coding-challenge/PopulationAPI/PopulationAPI/Controllers/LogController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json.Linq;
+using PopulationAPI.Data;
+using PopulationAPI.Model;
+using System.Text.Json;
+
+namespace PopulationAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class LogController : ControllerBase
+    {
+
+        private readonly LogsDBContext _context;
+        public LogController(LogsDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Log>> GetLogDetails() => await _context.Logs.ToListAsync();
+
+    }
+}
