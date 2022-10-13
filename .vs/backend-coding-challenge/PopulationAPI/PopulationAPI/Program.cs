6d7350a65ae08329842286f2b067a48a80d009a9
theMonadicNomad <hello@nagacharan.me>
2022-10-13 20:27:24 +0200

    added swagger and sql server indocker

--- .vs/backend-coding-challenge/PopulationAPI/PopulationAPI/Program.cs
+++ .vs/backend-coding-challenge/PopulationAPI/PopulationAPI/Program.cs
@@ -6,12 +6,16 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<LogsDBContext>(o => o.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer")));
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
+app.UseSwagger();
+app.UseSwaggerUI(c => {
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Population API V1");
+});
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
